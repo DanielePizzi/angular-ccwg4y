b53e5e451a96092327036a410d1d9a614539ef42
DanielePizzi <pizzi.daniele1993@gmail.com>
2022-06-06 14:49:58 +0200

    3 files modified, 3 added

--- src/app/app-routing-module.module.ts
+++ src/app/app-routing-module.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ThankyouComponent } from './thankyou/thankyou.component';
+import { VideoComponent } from '../video/video.component';
 
 const routes: Routes = [
+  {path:'', redirectTo: "/video", pathMatch: "full"},
+  {path:"video", component:VideoComponent},
   {path:"thanks", component:ThankyouComponent}
 ]
 
